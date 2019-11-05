e332d9c7d7e90fdf58c6ce76ce742c6aa63fef28
Matheus Santos Almeida <matheus.santosalmeida.ma9@gmail.com>
2019-11-05 13:03:52 -0300

    adição do campo comentário na avaliação

--- website/models.py
+++ website/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import F, Sum
+from django.db.models import F, Sum, Avg
 from django.utils import timezone
 from decimal import Decimal
 from django.contrib.auth.models import User
@@ -13,60 +13,66 @@ class ModelDate(models.Model):
     class Meta:
         abstract = True
 
+
 class Endereco(ModelDate):
-    UF = (('AC', 'Acre'), 
-            ('AL', 'Alagoas'),
-            ('AP', 'Amapá'),
-            ('AM', 'Amazonas'),
-            ('BA', 'Bahia'),
-            ('CE', 'Ceará'),
-            ('DF', 'Distrito Federal'),
-            ('ES', 'Espírito Santo'),
-            ('GO', 'Goiás'),
-            ('MA', 'Maranhão'),
-            ('MT', 'Mato Grosso'),
-            ('MS', 'Mato Grosso do Sul'),
-            ('MG', 'Minas Gerais'),
-            ('PA', 'Pará'),
-            ('PB', 'Paraíba'),
-            ('PR', 'Paraná'),
-            ('PE', 'Pernambuco'),
-            ('PI', 'Piauí'),
-            ('RR', 'Roraima'),
-            ('RO', 'Rondônia'),
-            ('RJ', 'Rio de Janeiro'),
-            ('RN', 'Rio Grande do Norte'),
-            ('RS', 'Rio Grande do Sul'),
-            ('SC', 'Santa Catarina'),
-            ('SP', 'São Paulo'),
-            ('SE', 'Sergipe'),
-            ('TO', 'Tocantins')
-            )
-     
+    UF = (('AC', 'Acre'),
+          ('AL', 'Alagoas'),
+          ('AP', 'Amapá'),
+          ('AM', 'Amazonas'),
+          ('BA', 'Bahia'),
+          ('CE', 'Ceará'),
+          ('DF', 'Distrito Federal'),
+          ('ES', 'Espírito Santo'),
+          ('GO', 'Goiás'),
+          ('MA', 'Maranhão'),
+          ('MT', 'Mato Grosso'),
+          ('MS', 'Mato Grosso do Sul'),
+          ('MG', 'Minas Gerais'),
+          ('PA', 'Pará'),
+          ('PB', 'Paraíba'),
+          ('PR', 'Paraná'),
+          ('PE', 'Pernambuco'),
+          ('PI', 'Piauí'),
+          ('RR', 'Roraima'),
+          ('RO', 'Rondônia'),
+          ('RJ', 'Rio de Janeiro'),
+          ('RN', 'Rio Grande do Norte'),
+          ('RS', 'Rio Grande do Sul'),
+          ('SC', 'Santa Catarina'),
+          ('SP', 'São Paulo'),
+          ('SE', 'Sergipe'),
+          ('TO', 'Tocantins')
+          )
+
     bairro = models.CharField('Bairro', max_length=50)
     rua = models.CharField('Rua', max_length=50)
     numero_casa = models.CharField('Número', max_length=5)
-    complemento = models.CharField('Complemento', max_length=50, blank=True, null=True)
+    complemento = models.CharField(
+        'Complemento', max_length=50, blank=True, null=True)
     cep = models.CharField('CEP', max_length=10)
     cidade = models.CharField('Cidade', max_length=120)
     uf = models.CharField('UF', max_length=2, choices=UF, default='AC')
-    
+
     def __str__(self):
         return self.cep
-    
+
+
 class Cliente(ModelDate):
     SEXO = (('M', 'Masculino'), ('F', 'Feminino'))
 
-    nome = models.CharField('Nome' ,max_length=50)
+    nome = models.CharField('Nome', max_length=50)
     sobrenome = models.CharField('Sobrenome', max_length=150)
     cpf = models.IntegerField('CPF', unique=True)
     rg = models.IntegerField('RG', unique=True, blank=True, null=True)
-    data_nascimento = models.DateField('Data de Nascimento', blank=True, null=True)
+    data_nascimento = models.DateField(
+        'Data de Nascimento', blank=True, null=True)
     sexo = models.CharField('Sexo', max_length=1, choices=SEXO, default='M')
-    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, related_name='clientes')
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cliente')
+    endereco = models.ForeignKey(
+        Endereco, on_delete=models.CASCADE, related_name='clientes')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='cliente')
 
-    @property   
+    @property
     def idade(self):
         now = timezone.now()
         idade = (timezone.now().date() - self.data_nascimento).days//365
@@ -74,70 +80,89 @@ class Cliente(ModelDate):
 
     def __str__(self):
         return self.nome + ' ' + self.sobrenome
-    
+
     class Meta:
         verbose_name = 'Cliente'
         verbose_name_plural = 'Clientes'
         ordering = ['nome']
 
+
 class Loja(ModelDate):
     nome_fantasia = models.CharField('Nome Fantasia', max_length=150)
     cnpj = models.IntegerField('CNPJ', unique=True)
-    razao_social = models.CharField('Razão Social', max_length=150, unique=True)
-    avaliacoes = models.ManyToManyField(Cliente, related_name='avaliacoes', through='Avaliacao')
+    razao_social = models.CharField(
+        'Razão Social', max_length=150, unique=True)
+    avaliacoes = models.ManyToManyField(
+        Cliente, related_name='avaliacoes', through='Avaliacao')
     logo = models.ImageField(
         upload_to='website/images', verbose_name='Imagem',
         null=True, blank=True)
 
+    @property
+    def rating(self):
+        return self.avaliacoes_loja.aggregate(rating=Avg('rating'))['rating']
+
     def __str__(self):
         return self.nome_fantasia
-    
+
     class Meta:
         verbose_name = 'Loja'
         verbose_name_plural = 'Lojas'
         ordering = ['nome_fantasia']
 
-        
+
 class Produto(ModelDate):
     descricao = models.CharField('Descrição', max_length=100)
     valor = models.DecimalField('Valor', max_digits=10, decimal_places=2)
-    loja = models.ForeignKey(Loja, on_delete=models.CASCADE, related_name='produtos')
+    loja = models.ForeignKey(
+        Loja, on_delete=models.CASCADE, related_name='produtos')
     qtd_estoque = models.IntegerField('Quantidade em estoque')
-    descricao_completa = models.TextField('Descrição Completa', blank=True, null=True)
+    descricao_completa = models.TextField(
+        'Descrição Completa', blank=True, null=True)
     logo = models.ImageField(
-            upload_to='website/images', verbose_name='Imagem',
-            null=True, blank=True)
+        upload_to='website/images', verbose_name='Imagem',
+        null=True, blank=True)
 
     def __str__(self):
         return self.descricao
-    
+
     class Meta:
         verbose_name = 'Produto'
         verbose_name_plural = 'Produtos'
         ordering = ['descricao']
 
+
 class Venda(ModelDate):
-    produtos = models.ManyToManyField(Produto, through='VendaProduto', related_name='vendas')
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, verbose_name='Cliente', related_name='vendas')
-    loja = models.ForeignKey(Loja, on_delete=models.CASCADE, related_name='vendas')
-    valor_total = models.DecimalField('Valor', max_digits=10, decimal_places=2, blank=True, default=Decimal('0.00'))
+    produtos = models.ManyToManyField(
+        Produto, through='VendaProduto', related_name='vendas')
+    cliente = models.ForeignKey(
+        Cliente, on_delete=models.CASCADE, verbose_name='Cliente', related_name='vendas')
+    loja = models.ForeignKey(
+        Loja, on_delete=models.CASCADE, related_name='vendas')
+    valor_total = models.DecimalField(
+        'Valor', max_digits=10, decimal_places=2, blank=True, default=Decimal('0.00'))
 
     def atualizar_valor(self):
-        expression = Sum(F('valor') * F('quantidade'), output_field=models.DecimalField(max_digits=10,decimal_places=2))
-        self.valor_total = self.vendas_produtos_venda.aggregate(valor_total=expression)['valor_total']
-        self.save()                                                                
+        expression = Sum(F('valor') * F('quantidade'),
+                         output_field=models.DecimalField(max_digits=10, decimal_places=2))
+        self.valor_total = self.vendas_produtos_venda.aggregate(
+            valor_total=expression)['valor_total']
+        self.save()
 
     def __str__(self):
         return str(self.pk) + ' - ' + str(self.cliente) + ' - ' + str(self.valor_total)
-    
+
     class Meta:
         verbose_name = 'Venda'
         verbose_name_plural = 'Vendas'
         ordering = ['-update_at']
 
+
 class VendaProduto(ModelDate):
-    venda = models.ForeignKey(Venda, on_delete=models.CASCADE, related_name='vendas_produtos_venda')
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name='vendas_produtos_produto')
+    venda = models.ForeignKey(
+        Venda, on_delete=models.CASCADE, related_name='vendas_produtos_venda')
+    produto = models.ForeignKey(
+        Produto, on_delete=models.CASCADE, related_name='vendas_produtos_produto')
     valor = models.DecimalField('Valor', max_digits=10, decimal_places=2)
     quantidade = models.PositiveIntegerField('Quantidade')
 
@@ -149,8 +174,11 @@ class VendaProduto(ModelDate):
         verbose_name_plural = 'Vendas de produtos'
         ordering = ['-update_at']
 
-class Avaliacao(ModelDate):
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='avaliacoes_cliente')
-    loja = models.ForeignKey(Loja, on_delete=models.CASCADE, related_name='avaliacoes_loja')
-    rating = models.FloatField('Rating')
 
+class Avaliacao(ModelDate):
+    cliente = models.ForeignKey(
+        Cliente, on_delete=models.CASCADE, related_name='avaliacoes_cliente')
+    loja = models.ForeignKey(
+        Loja, on_delete=models.CASCADE, related_name='avaliacoes_loja')
+    rating = models.FloatField('Rating')
+    comentario = models.TextField('Comentário', blank=True, null=True)
