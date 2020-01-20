15ccb290a8ba45a199b863caa4cd4e4491ca5d9c
Matheus Santos Almeida <matheus.santosalmeida.ma9@gmail.com>
2020-01-20 12:56:44 -0300

    xml

--- testedjango/settings.py
+++ testedjango/settings.py
@@ -119,14 +119,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '1/day',
-        'user': '1000/day'
-    },
     # 'DEFAULT_PERMISSION_CLASSES': (
     #    'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     # ),
